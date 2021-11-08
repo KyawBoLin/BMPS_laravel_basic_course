7ef48139bcd68f42bddc22f18df54c26239d1774
Kyaw Bo Lin <kyawbobolin1995@gmail.com>
2021-11-09 01:07:37 +0630

    get-post-middleware

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,6 +11,9 @@
 
 <h1>This is About Page</h1>
 
+{{-- <h5>Name is {{$name}}</h5>
+<h5>Age is {{$age}}</h5> --}}
+
 @endsection
 
 @section('script')
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\WebFrontend\aboutController;
 use App\Http\Controllers\WebFrontend\contactController;
 use App\Http\Controllers\WebFrontend\ProductController;
 use App\Http\Controllers\WebFrontend\shopController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('contact',[contactController::class,'contactPage']);
+Route::get('contact',[contactController::class,'contactPage'])->middleware('testMW');
 
 Route::get('about',[aboutController::class,'aboutPage']);
 
 Route::get('shop',[shopController::class,'shopPage']);
 
 Route::get('about/{product_id}/shop/{shop_id}',[ProductController::class,'productPage']);
+
+Route::get('promotions',[Controller::class,'showPromotions']);
+
+Route::post('api/postMethod',[Controller::class,'showPostMethod']);
